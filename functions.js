f22d0c2a21de750c754bf0de93f6cb52aa9e5e1b
jotaefe <juanfranruiz98@gmail.com>
2023-11-29 10:38:31 +0100

    Falta solo terminar guardar y actualizar

--- functions.js
+++ functions.js
@@ -12,17 +12,26 @@ const inputEntrenamiento = document.getElementById('entrenamiento');
 const inputDescripcion = document.getElementById('descripcion');
 const img = document.getElementById("img");
 const header = document.getElementById("header");
+const borrar = document.getElementById("borrar");
+const guardar = document.getElementById("guardar");
+const actualizar = document.getElementById("actualizar");
+const cambioUsuario = document.getElementById("cambioUsuario");
 
 //Declaracion de variables
 let dni;
 let nombre;
 let existeDni;
+let posicion;
+let actualizarFichero = () => {
+	fs.writeFileSync('./Usuarios.json', JSON.stringify(usuarios));
+}
 
 
 //Comprobacion del dni
 inputDni.addEventListener('keyup', (evento) => {
     //Cada vez que pulsa enter realiza las comprobaciones
     if(evento.key == "Enter"){
+        let contador = 0;
         existeDni=false;
         //Recorre todos los usuarios del json para comprobar si coincide con algun dni
         usuarios.forEach(function(object){
@@ -30,7 +39,9 @@ inputDni.addEventListener('keyup', (evento) => {
                 dni=object.dni;
                 nombre=object.nombre;
                 existeDni=true;
+                posicion=contador;
             }
+            contador++;
         })
         //Primero el formato
         if (existeDni) {
@@ -47,17 +58,49 @@ inputDni.addEventListener('keyup', (evento) => {
     }
     
 });
+
+//Accion al pulsar borrar
+//Borrar Fecha, Entreno, Descripcion
+borrar.addEventListener('click',()=>{
+    borrarCampos(false);
+});
+
+//Borra TODOS los inputs y bloquea
+cambioUsuario.addEventListener('click',()=>{
+    inputDni.value="";
+    borrarCampos(true);
+    cambiarEstado(true);
+});
+
+//Guarda los values en el JSON(FALTA POR MEJORAR)
+guardar.addEventListener('click',()=>{
+    usuarios[posicion].dni = inputDni.value
+	usuarios[posicion].nombre = inputNombre.value
+	usuarios[posicion].dia = inputFecha.value
+    usuarios[posicion].tipo_entrene = inputEntrenamiento.value
+    usuarios[posicion].descripcion = inputDescripcion.value
+	actualizarFichero();
+});
+
 //Activa o desactiva los inputs
 function cambiarEstado(estado){
     inputFecha.disabled = estado;
     inputEntrenamiento.disabled = estado;
     inputDescripcion.disabled = estado;
+    borrar.disabled = estado;
+    guardar.disabled = estado;
+    actualizar.disabled = estado;
+    cambioUsuario.disabled = estado;
 }
 
 //Borra todos los campos menos DNI
-//Si se pasa un false como parametro Nombre tampoco se borra
+//Si se pasa un false como parametro Nombre Img y Header no se borran
 function borrarCampos(condicion){
-    if(condicion)inputNombre.value="";
+    if(condicion){
+        img.src = "";
+        inputNombre.value="";
+        header.innerText="USUARIO";
+    }
     inputFecha.value = "";
     inputEntrenamiento.value = "";
     inputDescripcion.value = "";
